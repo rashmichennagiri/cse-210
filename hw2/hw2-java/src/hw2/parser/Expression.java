786de63837bca54af0dfbd956893064ad675f041
RashmiChennagiri <rchennag@ucsc.edu>
2020-04-22 04:09:58 -0700

    tool to generate parser class

--- hw2/hw2-java/src/hw2/parser/Expression.java
+++ hw2/hw2-java/src/hw2/parser/Expression.java
@@ -1,28 +1,77 @@
 package hw2.parser;
 
+import java.util.List;
+
 import hw2.lexer.Token;
+
 
 /**
  * 
  * @author rashmichennagiri
  *
  */
-public abstract class Expression {
+abstract class Expression {
+	
+	
+	/**
+	 * node for actual string/number/variable
+	 */
+	static class Literal extends Expression {
+
+		final Object value;
+
+		Literal(Object value) {
+			this.value = value;
+		}
+
+	}
 
 	
 	/**
-	 * 	Binary node
+	 * node for unary operation
 	 */
-	static class Binary extends Expression{
-		
+	static class Unary extends Expression {
+
+		final Token operator;
+		final Expression expr;
+
+		Unary(Token operator, Expression expr) {
+			this.operator = operator;
+			this.expr = expr;
+		}
+
+	}
+	
+	
+	/**
+	 * binary operation node
+	 */
+	static class Binary extends Expression {
+
 		final Expression left;
 		final Token operator;
 		final Expression right;
-		
-		Binary(Expression left, Token operator, Expression right){
+
+		Binary(Expression left, Token operator, Expression right) {
 			this.left = left;
 			this.operator = operator;
 			this.right = right;
 		}
+
 	}
+
+	
+	/**
+	 * node for nested expressions
+	 */
+	static class Grouping extends Expression {
+
+		final Expression expression;
+
+		Grouping(Expression expression) {
+			this.expression = expression;
+		}
+
+	}
+
 }
--- hw2/hw2-java/src/hw2/util/GenerateAST.java
+++ hw2/hw2-java/src/hw2/util/GenerateAST.java
@@ -1,5 +1,10 @@
 package hw2.util;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 
  * @author rashmichennagiri
@@ -7,4 +12,97 @@ package hw2.util;
  */
 public class GenerateAST {
 
+
+	/**
+	 * 
+	 * @param outputDirectory
+	 * @param baseName
+	 * @param types
+	 * @throws IOException
+	 */
+	private static void defineAST(String outputDirectory, String baseName, List<String> types) throws IOException {
+		
+		String path = outputDirectory + "/" + baseName + ".java";
+		PrintWriter pw = new PrintWriter( path, "UTF-8");
+		
+		pw.println("package hw2.parser;");
+		pw.println();
+		
+		pw.println("import java.util.List;");
+		pw.println();
+		
+		pw.println("abstract class " + baseName + "{");
+		
+		// AST classes
+		for(String type: types) {
+			String className = type.split(":")[0].trim();
+			String fields = type.split(":")[1].trim();
+			defineTypeSubClass(pw, baseName, className, fields);
+		}
+		
+		pw.println("}");
+		pw.close();
+		
+		
+	}	
+
+	
+	/**
+	 * 
+	 * @param pw
+	 * @param baseName
+	 * @param className
+	 * @param fields
+	 */
+	private static void defineTypeSubClass(PrintWriter pw, String baseName, String className, String fields) {
+		
+		pw.println("	static class " + className + " extends " + baseName + "{");
+		
+		//FIELDS
+		pw.println();
+		String[] fieldNames = fields.split(",");
+
+		for(String f: fieldNames) {
+			pw.println("	final " + f + ";");
+		}		
+		pw.println();
+		
+		//CONSTRUCTOR
+		pw.println("	" + className + "("+ fields + "){");
+		
+		//store parameters in fields
+		for(String f: fieldNames) {
+			String var = f.split(" ")[1];
+			pw.println("	this." + var + " = " + var + ";");
+		}
+		
+		pw.println("	}");
+		pw.println();
+		
+		pw.println("	}");
+		
+	}
+	
+	
+
+	/**
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		/*if(args.length!=1) {
+		      System.err.println("Usage: generate_ast <output directory>");
+		      System.exit(1);
+		}*/
+		
+		String outputDirectory = "/Users/rashmichennagiri/Documents/2020_q3_spring/cse210a_PL/pl_homeworks/cse-210/hw2/hw2-java/src/hw2/parser";
+		
+		defineAST( outputDirectory,
+				"Expression",
+				Arrays.asList("Binary : Expression left,Token operator,Expression right",
+					      "Grouping : Expression expression",                      
+					      "Literal  : Object value",                         
+					      "Unary    : Token operator,Expression expr"  ) );
+	}
 }
